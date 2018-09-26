bca7ada3e186f88b2343e44b1eb35c7d04e913ae
paul.bourdel <paul.bourdel@slalom.com>
2018-09-26 18:40:31 -0500

    adding nearby grocery stores

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   render() {
-    return <HelloWorld title="Hello from React webpack" />;
+    return <HelloWorld title="Nearby Grocery Stores" />;
   }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { compose, withProps } from 'recompose';
+import { compose, withHandlers, withProps, withState } from 'recompose';
 import {
   GoogleMap,
   Marker,
@@ -11,11 +11,6 @@ import App from './App.jsx';
 
 const MyMapComponent = compose(
   withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key 'AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q' can be ONLY used in this sandbox (no forked).
-     */
     googleMapURL:
       'https://maps.googleapis.com/maps/api/js?key=AIzaSyDop4rv216SLFFmCpyW2vwWyZhj-Gwgee0&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
@@ -24,16 +19,54 @@ const MyMapComponent = compose(
   }),
   withScriptjs,
   withGoogleMap,
+  withState('places', 'updatePlaces', ''),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    };
+
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref;
+      },
+      fetchPlaces: ({ updatePlaces }) => {
+        const bounds = refs.map.getBounds();
+        const service = new google.maps.places.PlacesService(
+          refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+        );
+        const request = {
+          bounds,
+          type: ['supermarket'],
+        };
+        service.nearbySearch(request, (results, status) => {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+            updatePlaces(results);
+          }
+        });
+      },
+    };
+  }),
 )(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    )}
+  <GoogleMap
+    onTilesLoaded={props.fetchPlaces}
+    ref={props.onMapMounted}
+    onBoundsChanged={props.fetchPlaces}
+    defaultZoom={16}
+    defaultCenter={{ lat: 41.8928403, lng: -87.6195965 }}
+  >
+    {props.places &&
+      props.places.map(place => (
+        <Marker
+          key={place.id}
+          position={{
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+          }}
+        />
+      ))}
   </GoogleMap>
 ));
 
-ReactDOM.render(
-  <MyMapComponent isMarkerShown />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<MyMapComponent />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('app'));
