aef4f276cdb87e1b4016307f91f446c173997463
paul.bourdel <paul.bourdel@slalom.com>
2018-09-27 11:10:41 -0500

    refactoring to separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import Geosuggest from 'react-geosuggest';
 import HelloWorld from './components/hello-world';
+import MyMapComponent from './components/my-map-component';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +11,18 @@ class App extends Component {
   }
 
   render() {
-    return <HelloWorld title="Nearby Grocery Stores" />;
+    return (
+      <>
+        <HelloWorld title="Nearby Grocery Stores" />
+        <Geosuggest
+          onSuggestSelect={suggest => {
+            console.log(suggest.location.lat);
+            console.log(suggest.location.lng);
+          }}
+        />
+        <MyMapComponent />
+      </>
+    );
   }
 }
 
---
+++ src/components/my-map-component/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose, withHandlers, withProps, withState } from 'recompose';
+import {
+  GoogleMap,
+  Marker,
+  withGoogleMap,
+  withScriptjs,
+} from 'react-google-maps';
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDop4rv216SLFFmCpyW2vwWyZhj-Gwgee0&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap,
+  withState('places', 'updatePlaces', ''),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    };
+
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref;
+      },
+      fetchPlaces: ({ updatePlaces }) => {
+        const bounds = refs.map.getBounds();
+        const service = new google.maps.places.PlacesService(
+          refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+        );
+        const request = {
+          bounds,
+          type: ['supermarket'],
+        };
+        service.nearbySearch(request, (results, status) => {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+            updatePlaces(results);
+          }
+        });
+      },
+    };
+  }),
+)(props => (
+  <GoogleMap
+    onTilesLoaded={props.fetchPlaces}
+    ref={props.onMapMounted}
+    onBoundsChanged={props.fetchPlaces}
+    defaultZoom={16}
+    defaultCenter={{ lat: 41.8928403, lng: -87.6195965 }}
+  >
+    {props.places &&
+      props.places.map(place => (
+        <Marker
+          key={place.id}
+          position={{
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+          }}
+        />
+      ))}
+  </GoogleMap>
+));
+
+MyMapComponent.propTypes = {
+  title: PropTypes.string,
+};
+
+export default MyMapComponent;
